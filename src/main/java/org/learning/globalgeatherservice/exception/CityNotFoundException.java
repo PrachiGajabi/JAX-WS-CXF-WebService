221e8e7debf884eeda319ec1e1f18a89023500e9
Prachi Gajabi <31819043+PrachiGajabi@users.noreply.github.com>
2017-10-08 21:58:10 +1100

    Add files via upload

---
+++ src/main/java/org/learning/globalgeatherservice/exception/CityNotFoundException.java
@@ -0,0 +1,17 @@
+package org.learning.globalgeatherservice.exception;
+
+public class CityNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1654240673586225827L;
+
+	public CityNotFoundException() {
+	}
+
+	public CityNotFoundException(String message) {
+		super(message);
+	}
+
+}
